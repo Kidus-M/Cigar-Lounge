dad00290044e431ec799eb7132968b76206f6f5f
Kidus Mesfin <kidus0237@gmail.com>
2025-01-31 19:08:24 +0300

    contacts edited and done

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -8,7 +8,7 @@ export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="fixed top-0 left-0 w-full bg-transparent z-50 text-white">
+    <nav className="fixed top-0 left-0 w-full bg-transparent z-50 text-white hover:bg-grey-800 transition-all">
       <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
         <Link href="/" className="text-3xl font-bold text-white hover:text-orange-600 transition-all">
           Cigar
--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -3,12 +3,12 @@ import Footer from "../../components/Footer";
 import Navbar from "../../components/Navbar";
 const Contacts = () => {
   return (
-    <div>
+    <div className="bg-black ">
       <Navbar />
-      <section class="mb-32">
+      <section class="bg-black mt-20">
         <div
           id="map"
-          class="relative h-[300px] overflow-hidden bg-cover bg-[50%] bg-no-repeat"
+          class="relative h-[300px] overflow-hidden bg-cover bg-[50%] bg-no-repeat bg-black"
         >
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11672.945750644447!2d-122.42107853750231!3d37.7730507907087!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80858070cc2fbd55%3A0xa71491d736f62d5c!2sGolden%20Gate%20Bridge!5e0!3m2!1sen!2sus!4v1619524992238!5m2!1sen!2sus"
@@ -16,29 +16,30 @@ const Contacts = () => {
             height="480"
             allowfullscreen=""
             loading="lazy"
+            className="opacity-70 hover:opacity-100"
           ></iframe>
         </div>
         <div class="container px-6 md:px-12">
-          <div class="block rounded-lg bg-[hsla(0,0%,100%,0.8)] px-6 py-12 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]  md:py-16 md:px-12 -mt-[100px] backdrop-blur-[30px] border border-gray-300">
+          <div class="block rounded-lg bg-black px-6 py-12 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]  md:py-16 md:px-12 -mt-[100px] backdrop-blur-[30px] border border-gray-300">
             <div class="flex flex-wrap">
               <div class="flex justify-center">
                 <div class="text-center md:max-w-xl lg:max-w-3xl">
-                  <h2 class="mb-12 px-6 text-3xl font-bold">Contact us</h2>
+                  <h2 class="mb-12 px-6 text-3xl text-orange-600 font-bold">Contact us</h2>
                 </div>
               </div>
 
               <div class="flex flex-wrap">
                 <form class="mb-12 w-full shrink-0 grow-0 basis-auto md:px-3 lg:mb-0 lg:w-5/12 lg:px-6">
-                  <div class="mb-3 w-full">
+                  <div class="mb-3 w-full ">
                     <label
-                      class="block font-medium mb-[2px] text-teal-700"
+                      class="block font-medium mb-[2px] text-orange-600"
                       htmlFor="exampleInput90"
                     >
                       Name
                     </label>
                     <input
                       type="text"
-                      class="px-2 py-2 border w-full outline-none rounded-md"
+                      class="px-2 py-2 border w-full bg-white outline-none rounded-md"
                       id="exampleInput90"
                       placeholder="Name"
                     />
@@ -46,14 +47,14 @@ const Contacts = () => {
 
                   <div class="mb-3 w-full">
                     <label
-                      class="block font-medium mb-[2px] text-teal-700"
+                      class="block font-medium mb-[2px] text-orange-600"
                       htmlFor="exampleInput90"
                     >
                       Email
                     </label>
                     <input
                       type="email"
-                      class="px-2 py-2 border w-full outline-none rounded-md"
+                      class="px-2 bg-white py-2 border w-full outline-none rounded-md"
                       id="exampleInput90"
                       placeholder="Enter your email address"
                     />
@@ -61,13 +62,13 @@ const Contacts = () => {
 
                   <div class="mb-3 w-full">
                     <label
-                      class="block font-medium mb-[2px] text-teal-700"
+                      class="block font-medium mb-[2px] text-orange-600"
                       htmlFor="exampleInput90"
                     >
                       Message
                     </label>
                     <textarea
-                      class="px-2 py-2 border rounded-[5px] w-full outline-none"
+                      class="px-2  bg-white py-2 border rounded-[5px] w-full outline-none"
                       name=""
                       id=""
                     ></textarea>
@@ -75,18 +76,18 @@ const Contacts = () => {
 
                   <button
                     type="button"
-                    class="mb-6 inline-block w-full rounded bg-teal-400 px-6 py-2.5 font-medium uppercase leading-normal text-white hover:shadow-md hover:bg-teal-500"
+                    class="mb-6 inline-block w-full rounded bg-orange-600 px-6 py-2.5 font-medium uppercase leading-normal text-white hover:shadow-md hover:bg-orange-700"
                   >
                     Send
                   </button>
                 </form>
 
                 <div class="w-full shrink-0 grow-0 basis-auto lg:w-7/12">
-                  <div class="flex flex-wrap">
+                  <div class="flex flex-wrap text-orange-600">
                     <div class="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:px-6">
                       <div class="flex items-start">
                         <div class="shrink-0">
-                          <div class="inline-block rounded-md bg-teal-400-100 p-4 text-teal-700">
+                          <div class="inline-block rounded-md bg-orange-600-100 p-4 text-orange-600">
                             <svg
                               xmlns="http://www.w3.org/2000/svg"
                               fill="none"
@@ -105,15 +106,15 @@ const Contacts = () => {
                         </div>
                         <div class="ml-6 grow">
                           <p class="mb-2 font-bold">Technical support</p>
-                          <p class="text-neutral-500 ">support@example.com</p>
-                          <p class="text-neutral-500 ">+1 234-567-89</p>
+                          <p class="text-white ">support@example.com</p>
+                          <p class="text-white ">+1 234-567-89</p>
                         </div>
                       </div>
                     </div>
                     <div class="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:px-6">
                       <div class="flex items-start">
                         <div class="shrink-0">
-                          <div class="inline-block rounded-md bg-teal-400-100 p-4 text-teal-700">
+                          <div class="inline-block rounded-md bg-orange-600-100 p-4 text-orange-600">
                             <svg
                               xmlns="http://www.w3.org/2000/svg"
                               fill="none"
@@ -132,15 +133,15 @@ const Contacts = () => {
                         </div>
                         <div class="ml-6 grow">
                           <p class="mb-2 font-bold ">Sales questions</p>
-                          <p class="text-neutral-500 ">sales@example.com</p>
-                          <p class="text-neutral-500 ">+1 234-567-89</p>
+                          <p class="text-white ">sales@example.com</p>
+                          <p class="text-white ">+1 234-567-89</p>
                         </div>
                       </div>
                     </div>
                     <div class="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:px-6">
                       <div class="align-start flex">
                         <div class="shrink-0">
-                          <div class="inline-block rounded-md bg-teal-400-100 p-4 text-teal-700">
+                          <div class="inline-block rounded-md bg-orange-600-100 p-4 text-orange-600">
                             <svg
                               xmlns="http://www.w3.org/2000/svg"
                               fill="none"
@@ -159,15 +160,15 @@ const Contacts = () => {
                         </div>
                         <div class="ml-6 grow">
                           <p class="mb-2 font-bold ">Press</p>
-                          <p class="text-neutral-500 ">press@example.com</p>
-                          <p class="text-neutral-500 ">+1 234-567-89</p>
+                          <p class="text-white ">press@example.com</p>
+                          <p class="text-white ">+1 234-567-89</p>
                         </div>
                       </div>
                     </div>
                     <div class="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:px-6">
                       <div class="align-start flex">
                         <div class="shrink-0">
-                          <div class="inline-block rounded-md bg-teal-400-100 p-4 text-teal-700">
+                          <div class="inline-block rounded-md bg-orange-600-100 p-4 text-orange-600">
                             <svg
                               xmlns="http://www.w3.org/2000/svg"
                               fill="none"
@@ -186,8 +187,8 @@ const Contacts = () => {
                         </div>
                         <div class="ml-6 grow">
                           <p class="mb-2 font-bold">Bug report</p>
-                          <p class="text-neutral-500 ">bugs@example.com</p>
-                          <p class="text-neutral-500">+1 234-567-89</p>
+                          <p class="text-white ">bugs@example.com</p>
+                          <p class="text-white">+1 234-567-89</p>
                         </div>
                       </div>
                     </div>
