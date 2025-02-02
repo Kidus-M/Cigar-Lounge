d5d44443a642274abb887848f1552080aafd7ab8
Kidus Mesfin <kidus0237@gmail.com>
2025-02-02 08:27:00 +0300

    small styling bug fix on contacts page

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -21,7 +21,7 @@ const Contacts = () => {
           ></iframe>
         </div>
         <div className="container px-6 md:px-12">
-          <div className="block rounded-lg bg-black px-6 py-12 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]  md:py-16 md:px-12 -mt-[100px] backdrop-blur-[30px] border border-2 border-orange-600">
+          <div className="block rounded-lg bg-black px-6 py-12 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]  md:py-16 md:px-12 -mt-[100px] backdrop-blur-[30px] border border-orange-600">
             <div className="flex flex-wrap">
               <div className="flex justify-center">
                 <div className="text-center md:max-w-xl lg:max-w-3xl">
