7b93adec4439b70c7452e19f8da42df7f59509d8
kidus-m <kidus0237@gmail.com>
2025-07-13 12:04:55 +0300

    removed the countdown from the main landing page

--- pages/index.js
+++ pages/index.js
@@ -57,7 +57,7 @@ export default function HeroSection() {
         {/* Decorative Elements */}
         <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-black to-transparent"></div>
       </section>
-        <Countdown />
+        {/*<Countdown />*/}
       
       {/* About Section */}
       
