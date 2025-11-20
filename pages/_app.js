12c3dafd6921a3f841f9e46ffe71ac05002efdbf
kidus-m <kidus0237@gmail.com>
2025-11-20 22:19:45 +0300

    added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -7,11 +7,14 @@ export default function App({ Component, pageProps }) {
   return<>
     <Head>
       <title>Wolf Den Lounge | Premium Cigars & Spirits</title>
-      <meta name="description" content="A modern sanctuary for cigar aficionados and spirit connoisseurs." />
-      <link rel="icon" href="/favicon.ico" />
+      <meta name="description" content="A modern sanctuary for cigar aficionados and spirit connoisseurs."/>
+      <link rel="icon" href="/favicon.ico"/>
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
     </Head>
-    <Navbar />
+    <Navbar/>
     <Component {...pageProps} />
-    <Footer />
-</>
+    <Footer/>
+  </>
 }
