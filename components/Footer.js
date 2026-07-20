ef827a3e79cb7acf9ffa203bcdef11921d9ca182
kidus-m <kidus0237@gmail.com>
2026-07-20 23:45:05 +0300

    changes 2

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
         <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
                     <div className="flex flex-col-reverse justify-between pt-5 pb-10 border-t lg:flex-row">
             <p className="text-sm text-white">
-              © 2026 Wolf Den Lounge. All rights reserved.
+              © 2024 Wolf Den Lounge. All rights reserved.
             </p>
           </div>
         </div>
